047a5c72a9d574ff598d0dd3f718e666289a73a8
Dale Torre <Dale Torre>
2016-11-14 20:48:38 +0800

    Added account list.

--- admin/user/list.php
+++ admin/user/list.php
@@ -1,37 +1,28 @@
 <?php
+$query = mysql_query("select * from user");
+
+?>
+
 	<table>
+	
 		<tr>
+			<th>Username</th>
 			<th>First Name</th>
 			<th>Last Name</th>
-			<th>Username</th>
 		</tr>
-		<tr>
-			<td>Peter</td>
-			<td>Griffin</td>
-			<td>username1</td>
-			<td><button>Delete</button></td>
-			<td><button>Update</button></td>
-		</tr>
-		<tr>
-			<td>Lois</td>
-			<td>Griffin</td>
-			<td>username2</td>
-			<td><button>Delete</button></td>
-			<td><button>Update</button></td>
-		</tr>
-		<tr>
-			<td>Joe</td>
-			<td>Swanson</td>
-			<td>username3</td>
-			<td><button>Delete</button></td>
-			<td><button>Update</button></td>
-		</tr>
-		<tr>
-			<td>Cleveland</td>
-			<td>Brown</td>
-			<td>username4</td>
-			<td><button>Delete</button></td>
-			<td><button>Update</button></td>
-		<tr>
-	</table>
+		
+<?php
+	while($row=mysql_fetch_array($query)){
 ?>
+		
+		<tr>
+			<td><?=$row['username']?></td>
+			<td><?=$row['fname']?></td>
+			<td><?=$row['lname']?></td>
+			<td><button>Delete</button></td>
+			<td><button>Update</button></td>
+		</tr>
+<?php
+	}
+?>
+	</table>
