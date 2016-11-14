6ed2e7e2ffcc5a2c9a55a1b4529a0ac357d33d29
Dale Torre <Dale Torre>
2016-11-14 17:02:03 +0800

    Added function for login.

--- user/index.php
+++ user/index.php
@@ -5,27 +5,11 @@ $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 
 switch ($view) {
 		
-	case 'list' :
+	case 'login' :
 		$content 	= 'list.php';
 		$template = '../include/template-main.php';		
 		break;
-		
-	case 'create' :
-		$content 	= 'create.php';
-		$template = '../include/template-main.php';		
-		break;
-		
-	case 'update' :
-		$content 	= 'update.php';
-		$template = '../include/template-main.php';		
-		break;
-		
-	case 'login' :
-		$content 	= 'login.php';
-		$template = '../include/template-main.php';		
-		break;
 			
-		
 	default :
 		$content 	= 'login.php';
 		$template = '../include/template-login.php';
