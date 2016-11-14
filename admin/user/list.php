81e9f16f5c9a89d46286cfbdf5551e26859a0708
Robert Carl Capapas <robert.capapas@esutek.com>
2016-11-14 20:40:32 +0800

    Created list

--- admin/user/list.php
+++ admin/user/list.php
@@ -1,3 +1,37 @@
 <?php
-
+	<table>
+		<tr>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Username</th>
+		</tr>
+		<tr>
+			<td>Peter</td>
+			<td>Griffin</td>
+			<td>username1</td>
+			<td><button>Delete</button></td>
+			<td><button>Update</button></td>
+		</tr>
+		<tr>
+			<td>Lois</td>
+			<td>Griffin</td>
+			<td>username2</td>
+			<td><button>Delete</button></td>
+			<td><button>Update</button></td>
+		</tr>
+		<tr>
+			<td>Joe</td>
+			<td>Swanson</td>
+			<td>username3</td>
+			<td><button>Delete</button></td>
+			<td><button>Update</button></td>
+		</tr>
+		<tr>
+			<td>Cleveland</td>
+			<td>Brown</td>
+			<td>username4</td>
+			<td><button>Delete</button></td>
+			<td><button>Update</button></td>
+		<tr>
+	</table>
 ?>
