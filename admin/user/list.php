b04224b42a569abd58333461a9914f687c89a152
Robert <robertcdc21@gmail.com>
2016-11-15 22:16:30 +0800

    Changed table design

--- admin/user/list.php
+++ admin/user/list.php
@@ -4,28 +4,85 @@ $query = mysql_query("select * from user");
 $message = (isset($_GET['message']) && $_GET['message'] != '') ? $_GET['message'] : '';
 ?>
 
-<br><?=$message;?>
-
-	<table>
-	
-		<tr>
-			<th>Username</th>
-			<th>First Name</th>
-			<th>Last Name</th>
-		</tr>
-		
-<?php
-	while($row=mysql_fetch_array($query)){
-?>
-		
-		<tr>
-			<td><?=$row['username']?></td>
-			<td><?=$row['fname']?></td>
-			<td><?=$row['lname']?></td>
-			<td><a href="process.php?action=delete&id=<?=$row['Id']?>">Delete</a></td>
-			<td><button>Update</button></td>
-		</tr>
-<?php
+<style>
+	table {
+		border-collapse: collapse;
+		width: 100%;
+		font-family: Arial;
+		font-size: 80%;
 	}
-?>
-	</table>
+
+	th, td {
+		padding: 8px;
+		text-align: center;
+		border-bottom: 1px solid #ddd;
+		text-decoration: none;
+	}
+	
+	th {
+		background-color: #b7b7b7;
+		color: white;
+	}
+
+	tr:hover{background-color:#f5f5f5}
+	
+	button {
+		border: none;
+		border-radius: 5px;
+		padding: 5px 10px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 90%;
+		margin: 2px 2px;
+		cursor: pointer;
+		-webkit-transition-duration: 0.4s; /* Safari */
+		transition-duration: 0.4s;
+	}
+	
+	.deletebtn:hover {
+		background-color: #ac1414;
+	}
+	
+	.deletebtn {
+		color: white;
+		background-color: #e52828;
+	}
+	
+	.updatebtn {
+		color: white;
+		background-color: #2391d6;
+	}
+	
+	.updatebtn:hover {
+		background-color: #186799;
+	}
+</style>
+
+<br><?=$message;?>
+	
+<table>
+	
+	<tr>
+		<th>Username</th>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th></th>
+		<th></th>
+	</tr>
+		
+	<?php
+		while($row=mysql_fetch_array($query)){
+	?>	
+		
+	<tr>
+		<td><?=$row['username']?></td>
+		<td><?=$row['fname']?></td>
+		<td><?=$row['lname']?></td>
+		<td><button class="button deletebtn" href="process.php?action=delete&id=<?=$row['Id']?>">Delete</button></td>
+		<td><button class="button updatebtn">Update</button></td>
+	</tr>
+	<?php
+		}
+	?>
+</table>
