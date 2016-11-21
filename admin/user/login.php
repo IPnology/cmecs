26eba4d803f1e91de5aa3d4d9e9f0661da193e69
agentyellow66 <johnlovaldez22@gmail.com>
2016-11-21 18:23:36 +0800

    Done coding for the new layout of log in form

--- admin/user/login.php
+++ admin/user/login.php
@@ -1,31 +1,16 @@
-<!DOCTYPE html>
 <?php
 $error = (isset($_GET['error']) && $_GET['error'] != '') ? $_GET['error'] : '';
 ?>
-<html>
-<body>
-
 <br><?=$error;?>
 
 <form action="process.php?action=login" method="POST">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Username" name="username">
-					</div>
-					<div class="form-group">
-						<input type="password" class="form-control" placeholder="Password" name="password">
-					</div>
-					<div class="form-group">
-						<input type="submit" class="btn btn-primary btn-md" value="LOG IN">
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+        
+    <button type="submit">Login</button>
 </form>
-	<a href="?view=register">Add Account</a>
-</body>
-</html>
