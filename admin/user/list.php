785616b52c4c0ed2d8a8bf867929db86ee2b8743
Dale Torre <Dale Torre>
2016-11-21 14:00:37 +0800

    Fix delete function.

--- admin/user/list.php
+++ admin/user/list.php
@@ -79,7 +79,7 @@ $message = (isset($_GET['message']) && $_GET['message'] != '') ? $_GET['message'
 		<td><?=$row['username']?></td>
 		<td><?=$row['fname']?></td>
 		<td><?=$row['lname']?></td>
-		<td><button class="button deletebtn" href="process.php?action=delete&id=<?=$row['Id']?>">Delete</button></td>
+		<td><button class="button deletebtn" onClick="location.href='process.php?action=delete&id=<?=$row['Id']?>'">Delete</button></td>
 		<td><button class="button updatebtn">Update</button></td>
 	</tr>
 	<?php
