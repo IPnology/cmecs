b53a7dc954028ba37deaa9f8a70b02d611e2a55c
ciagar23 <fredowinz23@gmail.com>
2016-11-21 17:20:51 +0800

    Added user name in the header

--- include/header.php
+++ include/header.php
@@ -7,7 +7,7 @@
 		</div>
 		<div class="h_icon">
 		<ul class="icon1 sub-icon1">
-			<li><a class="active-icon c1" href="#"><i>$300</i></a>
+			<li><a class="active-icon c1" href="#"><i><?=$_SESSION['user_session'];?></i></a>
 				<ul class="sub-icon1 list">
 					<li><h3>shopping cart empty</h3><a href=""></a></li>
 					<li><p>if items in your wishlit are missing, <a href="contact.html">contact us</a> to view them</p></li>
