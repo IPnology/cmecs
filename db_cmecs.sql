a9c613ae06fd0786f34530daa1bb9c612894d08b
ciagar23 <fredowinz23@gmail.com>
2016-11-21 14:10:07 +0800

    Added category database

--- db_cmecs.sql
+++ db_cmecs.sql
@@ -1,8 +1,25 @@
 # Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
-# Date: 2016-11-14 14:50:03
+# Date: 2016-11-21 14:08:28
 # Generator: MySQL-Front 5.4  (Build 1.4)
 
 /*!40101 SET NAMES latin1 */;
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "category"
+#
+
+INSERT INTO `category` VALUES (1,'cat1'),(2,'cat2');
 
 #
 # Structure for table "user"
