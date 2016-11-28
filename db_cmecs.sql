1ad198ecd21d469d3b83e4c8d77382112e2184ba
ciagar23 <fredowinz23@gmail.com>
2016-11-28 18:10:28 +0800

    update database

--- db_cmecs.sql
+++ db_cmecs.sql
@@ -1,8 +1,29 @@
-﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
-# Date: 2016-11-28 17:20:54
-# Generator: MySQL-Front 5.4  (Build 1.40)
+# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
+# Date: 2016-11-28 18:10:09
+# Generator: MySQL-Front 5.4  (Build 1.4)
 
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES latin1 */;
+
+#
+# Structure for table "admin"
+#
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+#
+# Data for table "admin"
+#
+
+INSERT INTO `admin` VALUES (1,'admin','i am','admin','1234','admin'),(2,'dalelicious','dale','torre','123','client'),(3,'fredowinz23','fred','garcia','123','client');
 
 #
 # Structure for table "cart"
@@ -44,11 +65,11 @@ CREATE TABLE `category` (
 INSERT INTO `category` VALUES (1,'cat1','','1480310724.jpg'),(2,'cat2','','1480310734.jpg'),(4,'cat4','cat ni sa','1480310743.jpg');
 
 #
-# Structure for table "order"
+# Structure for table "checkout"
 #
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `checkout`;
+CREATE TABLE `checkout` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(11) DEFAULT NULL,
   `orderNumber` varchar(11) DEFAULT NULL,
@@ -57,13 +78,38 @@ CREATE TABLE `order` (
   `status` varchar(11) DEFAULT NULL,
   `totalPrice` float DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 #
-# Data for table "order"
+# Data for table "checkout"
 #
 
-INSERT INTO `order` VALUES (1,'dalelicious','1','bacolod','1-1-2017','delivered',100);
+INSERT INTO `checkout` VALUES (1,'dalelicious','1','bacolod','1-1-2017','delivered',100);
+
+#
+# Structure for table "item"
+#
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `productId` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user` varchar(100) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `current` int(10) DEFAULT '0',
+  `ordered` int(10) DEFAULT '0',
+  `sold` int(10) DEFAULT '0',
+  `month` varchar(2) DEFAULT NULL,
+  `year` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+#
+# Data for table "item"
+#
+
+INSERT INTO `item` VALUES (1,4,'insert','fredowinz23','2017-05-18 02:30:09',100,100,4,'05','2017'),(2,4,'insert','fredowinz23','2017-06-15 02:30:35',136,40,3,'06','2017'),(3,4,'insert','fredowinz23','2017-07-13 02:31:04',133,0,70,'07','2017'),(4,4,'insert','fredowinz23','2017-08-17 02:31:34',163,100,50,'08','2017'),(5,4,'insert','fredowinz23','2017-09-14 02:32:17',113,0,5,'09','2017'),(6,4,'insert','fredowinz23','2017-10-13 02:32:44',208,100,30,'10','2017'),(7,4,'insert','fredowinz23','2017-11-17 02:33:26',208,30,60,'11','2017'),(8,4,'insert','fredowinz23','2017-12-15 02:34:00',208,60,60,'12','2017'),(9,4,'insert','fredowinz23','2018-01-18 02:34:57',188,40,0,'01','2018'),(10,4,'insert','fredowinz23','2018-02-15 02:36:08',199,11,140,'02','2018'),(11,5,'insert','fredowinz23','2016-11-27 19:57:53',30,10,0,'11','2016'),(12,5,'insert','fredowinz23','2016-12-01 19:59:14',1035,1000,0,'12','2016'),(14,5,'insert','fredowinz23','2017-03-01 23:54:49',9824,8789,40,'03','2017');
 
 #
 # Structure for table "product"
@@ -98,13 +144,13 @@ CREATE TABLE `temp_cart` (
   `quantity` int(11) DEFAULT '1',
   `price` float DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 #
 # Data for table "temp_cart"
 #
 
-INSERT INTO `temp_cart` VALUES (11,'dalelicious',2,2,0),(12,'dalelicious',2,1,0),(13,'dalelicious',5,1,600);
+INSERT INTO `temp_cart` VALUES (11,'dalelicious',2,2,0),(12,'dalelicious',2,1,0),(13,'dalelicious',5,1,600),(14,'admin',1,1,0);
 
 #
 # Structure for table "user"
@@ -119,7 +165,7 @@ CREATE TABLE `user` (
   `password` varchar(255) DEFAULT NULL,
   `level` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 #
 # Data for table "user"
