0b02fc7c04ae787893c8285cc205e460af1887f0
ciagar23 <fredowinz23@gmail.com>
2016-11-28 16:39:40 +0800

    fixed customer header location

--- customer/category/update.php
+++ customer/category/update.php
@@ -5,17 +5,17 @@ $query = mysql_query("select * from category where Id = $id");
 
 $row=mysql_fetch_array($query)
 ?>
+
 <form action="process.php?action=update&id=<?=$id?>" method="POST" enctype="multipart/form-data">
+	<div class="ccontainer" style="border:1px solid; padding:10px 20px; margin-left:500px;">
 	Name:</br>
-	<div style="width:500px; padding: 6px 10px;">
-	<input type="text" name="name" value="<?=$row['name']?>" placeholder="Add category"></br>
-	</div>
-	<div style="width:500px; padding: 6px 10px;">
+	<input type="text" style="width:350px;" name="name" value="<?=$row['name']?>" placeholder="Add category"></br>
 	Description:</br>
-	<textarea name="description"><?=$row['description']?></textarea></br>
-	</div>
-	Upload Photo:</br>
-	<input type="file" name="upload_file"  /><br>
+	<textarea name="description" style="width:350px; height:100px; padding:6px 10px;"><?=$row['description']?></textarea></br></br>
+	Upload Photo:
+	<input type="file" name="upload_file"/><br>
 	</br></br>
-	<input type="submit" value="Update">
+	<button class="button updatebtn" type="submit">Save Changes</button>
+	
+	</div>
 </form>
--- customer/product/index.php
+++ customer/product/index.php
@@ -8,42 +8,42 @@ switch ($view) {
 	case 'add' :
 		$content = 'add.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';
 		break;
 		
 	case 'list' :
 		$content 	= 'list.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';		
 		break;
 		
 	case 'update' :
 		$content 	= 'update.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';		
 		break;
 		
 	case 'detail' :
 		$content 	= 'detail.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';		
 		break;
 		
 	case 'searchList' :
 		$content 	= 'searchList.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';		
 		break;
 		
 	case 'temp-cart' :
 		$content 	= 'tempCart.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';		
 		break;
 		
@@ -51,7 +51,7 @@ switch ($view) {
 	default :
 		$content 	= 'list.php';
 		$template = '../../include/template-main.php';		
-		$header 	= '../../include/header.php';
+		$header 	= '../../include/headerCustomer.php';
 		$footer 	= '../../include/footer.php';
 }
 
