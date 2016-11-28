a62c6814b6862a867c10635cfff13921b393d6cb
Dale Torre <Dale Torre>
2016-11-28 20:26:41 +0800

    Add myaccount.

---
+++ user/account/detail.php
@@ -0,0 +1,21 @@
+<?php
+$username = $_SESSION['customer_session'];
+
+$query = mysql_query("select * from user where username = '$username'");
+
+$row=mysql_fetch_array($query);
+?>
+
+		Username: <?=$row['username']?></br></br>
+		First Name: <?=$row['fname']?></br></br>
+		Last Name: <?=$row['lname']?></br></br>
+		Email: <?=$row['email']?></br></br>
+		Birthdate: <?=$row['birthdate']?></br></br>
+		Gender: <?=$row['gender']?></br></br>
+		Street: <?=$row['street']?></br></br>
+		Barangay: <?=$row['brgy']?></br></br>
+		City: <?=$row['city']?></br></br>
+		Province: <?=$row['province']?></br></br>
+		Country: <?=$row['country']?></br></br>
+		Postal Code: <?=$row['postal']?></br></br>
+		<button class="button updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">Update</button>	
