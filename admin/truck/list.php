961b31aa9d59fb7f0f6becbacbdb6685ce0032a2
Robert <robertcdc21@gmail.com>
2016-12-06 01:21:18 +0800

    Edited truck table and search list

--- admin/truck/list.php
+++ admin/truck/list.php
@@ -5,23 +5,28 @@ $query = mysql_query("select * from truck");
 
 $message = (isset($_GET['message']) && $_GET['message'] != '') ? $_GET['message'] : '';
 ?>
+<div class="listwrapper">
+	<button class="myButton" onClick="location.href='?view=add'">Add Truck</button></br>
+	<div class="successmessage"><?=$message;?></div>
 
-<br><?=$message;?>
+	<table class="tablelist">
+	<tr>
+		<th>TRUCK NAME</th>
+		<th></th>
+		<th></th>
+	</tr> 
+	<?php
+		while($row=mysql_fetch_array($query)){
+	?>
+	<tr>
+		<td><?=$row['name']?></td>
+		<td><button class="button updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">Update</button></td>
+		<td><button class="button deletebtn" onClick="location.href='process.php?action=delete&id=<?=$row['Id']?>'">Delete</button></td>
+	</tr>
 
-<div class="main_bg">
-<div class="wrap">	
-<div class="main">	
-<button onClick="location.href='?view=add'">Add Truck</button>
-<?php
-	while($row=mysql_fetch_array($query)){
-?>
-Name: <?=$row['name']?>
-<button class="button updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">Update</button>
-<button class="button deletebtn" onClick="location.href='process.php?action=delete&id=<?=$row['Id']?>'">Delete</button></br></br>
-<?php
-	}
-?>
-<div class="clear"></div>
+	<?php
+		}
+	?>
+	</table>
+	<div class="clear"></div>
 </div>
-</div>
-</div>	
