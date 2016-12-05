4c8521a42927a61d2221ed3aa506a9fe04ed6db0
Dale Torre <Dale Torre>
2016-12-05 19:46:49 +0800

    Delivery

--- admin/order/setDelivery.php
+++ admin/order/setDelivery.php
@@ -1,12 +1,15 @@
 <?php
 $id = $_GET['id'];
 $query = mysql_query("select * from checkout where Id=$id");
+$truckQuery = mysql_query("select * from truck");
 $row = mysql_fetch_array($query);
 $orderNumber = $row['orderNumber'];
 $cartQuery = mysql_query("select * from cart where orderNumber='$orderNumber'");
 $message = (isset($_GET['message']) && $_GET['message'] != '') ? $_GET['message'] : '';
 ?>
+
 <?=$message;?></br></br>
+
 FOR DELIVERY ORDER</br></br>
 <?php
 if(mysql_num_rows($cartQuery)>0){
@@ -34,3 +37,22 @@ Address:<?=$row['street'];?>, <?=$row['brgy'];?>, <?=$row['city'];?>, <?=$row['p
 Date:<?=$row['date'];?></br></br>
 Total Price:<?=$row['totalPrice'];?></br></br>
 Status:<?=$row['status'];?></br></br>
+
+<form action="process.php?action=setDelivery&id=<?=$id?>" method="POST">
+<input type="hidden" name="orderNumber" value="<?=$row['orderNumber'];?>">
+<input type="date" name="deliveryDate">
+<label>CHOOSE TRUCK *</label></br></br>
+<select name="truck" class="select"required>
+	<option value="">-- Select Truck --</option>
+	<?php
+	if(mysql_num_rows($truckQuery)>0){ 
+			while($truckRow=mysql_fetch_array($truckQuery)){
+	?>
+	<option value="<?=$truckRow['Id']?>"><?=$truckRow['name']?></option>
+	<?php
+			}
+	}
+	?>
+</select>
+<input type="submit" class="myButton" value="Set Delivery">
+</form>
