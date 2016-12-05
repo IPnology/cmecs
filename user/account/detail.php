938ba0bf8623a52720722a0042691d1146197601
Robert <robertcdc21@gmail.com>
2016-12-05 19:51:45 +0800

    Fixed content

--- user/account/detail.php
+++ user/account/detail.php
@@ -6,22 +6,18 @@ $row=mysql_fetch_array($query);
 
 <div class="accountdetails">
 	<div class="myaccountheader">Personal Information</div></br>
-		
-		<div class="myaccountfont"><?=$row['fname']?> <?=$row['lname']?></br></br>
-		<?=$row['street']?></br></br>, <?=$row['brgy']?></br></br>
-		<?=$row['city']?></br></br>
-		<?=$row['province']?></br></br>
-		<?=$row['postal']?></br></br>
-		
+	<div class="myaccountfont"><?=$row['fname']?> <?=$row['lname']?></br></br>
 		Birthdate: <?=$row['birthdate']?></br></br>
 		Gender: <?=$row['gender']?></br></br>
 		Phone Number: <?=$row['phoneNumber']?></br></br>
-		
-		
-		
 		Email: <?=$row['email']?></br></br>
-		
-		Username: <?=$row['username']?></br></br>
-		</div>
-		<div class="accountbtn"><button class="updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">Update</button></div>
+		Username: <?=$row['username']?></br></br></br>
+	<div class="myaccountheader">Shipping Address</div></br>
+		<?=$row['street']?>, <?=$row['brgy']?></br></br>
+		<?=$row['city']?></br></br>
+		<?=$row['province']?></br></br>
+		<?=$row['postal']?></br></br>
+	</div>
+	</br>
+	<button class="updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">UPDATE</button>
 </div>
--- user/home/content.php
+++ user/home/content.php
@@ -1,28 +1,28 @@
 <!-- start slider -->
 			<div id="da-slider" class="da-slider">
 				<div class="da-slide">
-					<h2>welcome to aditii</h2>
-					<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+					<h2>welcome to Casa Muebles</h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider1.png" alt="image01" /></div>
+					<div class="da-img"><img src="../../include/images/slider1.png" alt="image01" /></div>
 				</div>
 				<div class="da-slide">
-					<h2>Easy management</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+					<h2>welcome to Casa Muebles</h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider2.png" alt="image01" /></div>
+					<div class="da-img"><img src="../../include/images/slider2.png" alt="image01" /></div>
 				</div>
 				<div class="da-slide">
-					<h2>Revolution</h2>
-					<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+					<h2>welcome to Casa Muebles</h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider3.png" alt="image01" /></div>
+					<div class="da-img"><img src="../../include/images/slider3.png" alt="image01" /></div>
 				</div>
 				<div class="da-slide">
-					<h2>Quality Control</h2>
-					<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+					<h2>welcome to Casa Muebles</h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 					<a href="details.html" class="da-link">shop now</a>
-					<div class="da-img"><img src="images/slider4.png" alt="image01" /></div>
+					<div class="da-img"><img src="../../include/images/slider4.png" alt="image01" /></div>
 				</div>
 				<nav class="da-arrows">
 					<span class="da-arrows-prev"></span>
@@ -36,7 +36,7 @@
 	<div id="owl-demo" class="owl-carousel">
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c1.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded shoes</a></h4>
@@ -45,7 +45,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -54,7 +54,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c3.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded jeens</a></h4>
@@ -63,7 +63,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -72,7 +72,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c1.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded shoes</a></h4>
@@ -81,7 +81,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c2.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded tees</a></h4>
@@ -90,7 +90,7 @@
 		</div>	
 		<div class="item" onclick="location.href='details.html';">
 			<div class="cau_left">
-				<img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+				<img class="lazyOwl" data-src="../../include/images/c3.jpg" alt="Lazy Owl Image">
 			</div>
 			<div class="cau_left">
 				<h4><a href="details.html">branded jeens</a></h4>
@@ -99,87 +99,4 @@
 		</div>	
 	</div>
 	<!----//End-img-cursual---->
-</div>
-<!-- start main1 -->
-<div class="main_bg1">
-<div class="wrap">	
-	<div class="main1">
-		<h2>featured products</h2>
-	</div>
-</div>
-</div>
-<!-- start main -->
-<div class="main_bg">
-<div class="wrap">	
-	<div class="main">
-		<!-- start grids_of_3 -->
-		<div class="grids_of_3">
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic1.jpg" alt=""/>
-					<h3>branded shoes</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic2.jpg" alt=""/>
-					<h3>branded t-shirts</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic3.jpg" alt=""/>
-					<h3>branded tees</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="clear"></div>
-		</div>
-		<div class="grids_of_3">
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic4.jpg" alt=""/>
-					<h3>branded bags</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic5.jpg" alt=""/>
-					<h3>ems women bag</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="grid1_of_3">
-				<a href="details.html">
-					<img src="images/pic6.jpg" alt=""/>
-					<h3>branded cargos</h3>
-					<div class="price">
-						<h4>$300<span>indulge</span></h4>
-					</div>
-					<span class="b_btm"></span>
-				</a>
-			</div>
-			<div class="clear"></div>
-		</div>	
-		<!-- end grids_of_3 -->
-	</div>
-</div>	
 </div>
