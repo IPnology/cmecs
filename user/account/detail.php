84dd954527b2315a96f85820e35a5a99019c278b
Robert <robertcdc21@gmail.com>
2016-12-08 23:52:26 +0800

    fixed account details

--- user/account/detail.php
+++ user/account/detail.php
@@ -5,19 +5,39 @@ $row=mysql_fetch_array($query);
 ?>
 
 <div class="accountdetails">
-	<div class="myaccountheader">Personal Information</div></br>
-	<div class="myaccountfont"><?=$row['fname']?> <?=$row['lname']?></br></br>
-		Birthdate: <?=$row['birthdate']?></br></br>
-		Gender: <?=$row['gender']?></br></br>
-		Phone Number: <?=$row['phoneNumber']?></br></br>
-		Email: <?=$row['email']?></br></br>
-		Username: <?=$row['username']?></br></br></br>
-	<div class="myaccountheader">Shipping Address</div></br>
-		<?=$row['street']?>, <?=$row['brgy']?></br></br>
-		<?=$row['city']?></br></br>
-		<?=$row['province']?></br></br>
-		<?=$row['postal']?></br></br>
+
+	<div class="personalinfo">
+		<div class="myaccountheader" style=" font-size: 1.6em;">Personal Information</div></br>
+		<div class="myaccountfont">
+			<div style="font-size: 1.3em;"><?=$row['fname']?> <?=$row['lname']?></div></br>
+			<div class="accountlabels">Birthdate: &nbsp;</div><?=$row['birthdate']?></br></br>
+			<div class="accountlabels">Gender: &nbsp;</div><?=$row['gender']?></br></br>
+			<div class="accountlabels">Phone Number: &nbsp;</div><?=$row['phoneNumber']?></br></br>
+			<div class="accountlabels">E-mail: &nbsp;</div><?=$row['email']?></br></br>
+			<div class="accountlabels">Username: &nbsp;</div><?=$row['username']?></br></br>
+		</div>
 	</div>
 	</br>
-	<button class="updatebtn" onClick="location.href='?view=update&id=<?=$row['Id']?>'">UPDATE</button>
+	<div class="accountleft">
+		<div class="myaccountheader">Billing Address</div></br>
+		<div class="myaccountfont">
+			<?=$row['street']?>, <?=$row['brgy']?></br></br>
+			<?=$row['city']?></br></br>
+			<?=$row['province']?></br></br>
+			<?=$row['postal']?></br></br>
+			<button class="myButton" onClick="location.href='?view=update&id=<?=$row['Id']?>'">UPDATE</button>
+		</div>
+	</div>
+		
+	<div class="accountleft">	
+		<div class="myaccountheader">Shipping Address</div></br>
+		<div class="myaccountfont">
+			<?=$row['street']?>, <?=$row['brgy']?></br></br>
+			<?=$row['city']?></br></br>
+			<?=$row['province']?></br></br>
+			<?=$row['postal']?></br></br>
+			<button class="myButton" onClick="location.href='?view=update&id=<?=$row['Id']?>'">UPDATE</button>
+		</div>
+	</div>
+	<div class="clear"></div>
 </div>
