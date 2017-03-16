2c9e52594609ea71d7982358ae7517c2d3b8ec20
ciagar23 <fredowinz23@gmail.com>
2017-03-16 20:53:07 +0800

    added config for hosting

--- config/database.php
+++ config/database.php
@@ -3,4 +3,13 @@ session_start();
 $connect = @mysql_connect("localhost","root","") or die("Server is terrible");
 mysql_select_db("db_cmecs",$connect) or die("i cant connect to it");
 
+/*
+
+session_start();
+$connect = @mysql_connect("sql109.epizy.com","epiz_19667127","ZXasqw1212") or die("Server is terrible");
+mysql_select_db("epiz_19667127_cmecs",$connect) or die("i cant connect to it");
+
+
+*/
+
 ?>
